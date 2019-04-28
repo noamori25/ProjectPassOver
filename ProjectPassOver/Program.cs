453b7a3dfa4b231f144395b51011a9aac71bc2b9
noamori25 <44751354+noamori25@users.noreply.github.com>
2019-04-28 20:10:54 +0300

    Add files via upload

---
+++ ProjectPassOver/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPassOver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+           // c.DeleteResults();
+           // c.DeleteYAndX();
+           // c.InsertXAndY();
+            // c.CrossJoinIntoResultsTable();
+            c.UpdateResultsColumnInResultsTable();
+        }
+    }
+}
